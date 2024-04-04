d77e708170c33ffbb835052e57b4532d4f4f1f5d
arafat-sabbir <arafatshabbir8@gmail.com>
2024-04-04 21:33:25 +0600

    Learned Aretmetic operator in c Programming

---
+++ arithmetic.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    printf("%d", sum);
+    int c = 50;
+    int d = 30;
+    int different = c - d;
+    printf("%d", different);
+    int e = 10;
+    int f = 20;
+    int multiply = e * f;
+    printf("%d", multiply);
+    int g = 50;
+    int h = 5;
+    int division = g / h;
+    printf("%d", division);
+    int i = 18;
+    int j = 5;
+    int modulas = i%j;
+    printf("%d",modulas);
+    return 0;
+}
