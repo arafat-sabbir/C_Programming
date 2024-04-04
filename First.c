850c5194ab940a72f0f9c26845ae6603cc04613f
arafat-sabbir <arafatshabbir8@gmail.com>
2024-04-04 21:26:07 +0600

    Learned How To Take Input And Store them on variable

--- First.c
+++ First.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    printf("hello ");
+    int i;
+    printf("hello");
+    printf("this is from my first c programm");
+    scanf("%d", &i);
+    printf("%d", i);
     return 0;
 }
