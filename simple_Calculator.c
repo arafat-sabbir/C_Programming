342acb4b3665b1fec0ad9079ae8f707a8d073a92
arafat-sabbir <arafatshabbir8@gmail.com>
2024-04-05 09:54:29 +0600

    Explored A Simple Calculator With Adition  Subtraction And Multificitaion And Division

---
+++ simple_Calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a , b;
+    scanf("%d %d",&a,&b);
+    int sum = a+b;
+    printf("%d + %d = %d\n",a,b,sum);
+    int mul = a*b;
+    printf("%d * %d = %d\n",a,b,mul); 
+    int div = a/b;
+    printf("%d / %d = %d\n",a,b,div); 
+    return 0;
+}
