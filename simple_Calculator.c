ae290e15c2c6fd22aa9e77aeb8067bdcaa6a5421
arafat-sabbir <arafatshabbir8@gmail.com>
2024-04-05 09:59:42 +0600

    Used Long Long Int because in multification int doesn't suupor 10to the power 10

--- simple_Calculator.c
+++ simple_Calculator.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int a , b;
+    long long int a , b;
     scanf("%d %d",&a,&b);
     int sum = a+b;
     printf("%d + %d = %d\n",a,b,sum);
     int sub = a-b;
     printf("%d - %d = %d\n",a,b,sub);
-    int mul = a*b;
-    printf("%d * %d = %d\n",a,b,mul); 
+    long long int mul = a*b;
+    printf("%llf * %llf = %llf\n",a,b,mul); 
     int div = a/b;
     printf("%d / %d = %d\n",a,b,div); 
     return 0;
