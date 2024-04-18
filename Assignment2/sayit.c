5bb0ee85549de1eac4522f87934e86d7a9db3f2c
arafat-sabbir <arafatshabbir8@gmail.com>
2024-04-18 23:09:14 +0600

    Solved Say it Problem

---
+++ Assignment2/sayit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    long long int a;
+    scanf("%lld", &a);
+    for (int i = 0; i < a; i++)
+    {
+        printf("%d.\tI Want More Assignments\n",i+1);
+    }
+    return 0;
+}
