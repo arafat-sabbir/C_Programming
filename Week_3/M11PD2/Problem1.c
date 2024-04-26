607ce44841842500bb632fce2fce2dc5e7d5b0d3
arafat-sabbir <arafatshabbir8@gmail.com>
2024-04-26 23:55:53 +0600

    Solved Module 11 Practice Day 2 Problem 1

---
+++ Week_3/M11PD2/Problem1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char x[100], y[100];
+    scanf("%s %s", x, y);
+    int i = 0;
+    while (1)
+    {
+        if (x[i] == '\0' && y[i] == '\0')
+        {
+            printf("%s", x);
+            break;
+        }
+        else if (y[i] == '\0')
+        {
+            printf("%s", y);
+            break;
+        }
+        else if (x[i] == '\0')
+        {
+            printf("%s", x);
+            break;
+        }
+
+        if (x[i] == y[i])
+        {
+            i++;
+        }
+        else if (x[i] < y[i])
+        {
+            printf("%s", x);
+            break;
+        }
+        else if (y[i] < x[i])
+        {
+            printf("%s", y);
+            break;
+        }
+    }
+    return 0;
+}
