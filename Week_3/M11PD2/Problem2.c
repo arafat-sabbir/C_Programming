00d561bc351524aba10463f3f36163bf00ca0584
arafat-sabbir <arafatshabbir8@gmail.com>
2024-04-27 00:04:14 +0600

    Solved Problem 2

---
+++ Week_3/M11PD2/Problem2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        char digit;
+        scanf(" %c", &digit); 
+        sum += digit - '0';
+    }
+    printf("%d", sum);
+    return 0;
+}
