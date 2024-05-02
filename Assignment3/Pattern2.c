6b2140a7261c6586e762a744b763f33594f7f447
arafat-sabbir <arafatshabbir8@gmail.com>
2024-05-02 21:41:10 +0600

    Solved Problem 2

---
+++ Assignment3/Pattern2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n; // Change this value to the desired number of rows
+    scanf("%d",&n);
+
+    // Outer loop for rows
+    for (int i = 1; i <= n; i++) {
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        // Print numbers in decreasing order
+        for (int j = i; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
